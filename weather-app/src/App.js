10a1895062398744efe8ca4acac1c1aa5c1fa6e1
Ida Sofie Grude <sofie.grude@gmail.com>
2024-03-21 10:20:20 +0100

    la til overskrift

--- weather-app/src/App.js
+++ weather-app/src/App.js
@@ -1,34 +1,33 @@
-
-
 // App.js
-import React, { useState } from 'react';
-import SearchBox from './components/search-box';
-import WeatherCard from './components/weather-card';
+import React, { useState } from "react";
+import SearchBox from "./components/search-box";
+import WeatherCard from "./components/weather-card";
 
 function App() {
-    const [weatherDataList, setWeatherDataList] = useState([]);
+  const [weatherDataList, setWeatherDataList] = useState([]);
 
-    function handleWeatherData(data) {
-        console.log(data[0]);
-        setWeatherDataList(oldList => [...oldList, ...data]);
-    }
-    return (
-        <div className='app'>
-            <SearchBox onWeatherData={handleWeatherData} />
-            <div className='card-container'>
-                {weatherDataList.map((weatherData, index) => (
-                    <WeatherCard 
-                        key={index} 
-                        city={weatherData.cityname} 
-                        temp={weatherData.temp} 
-                        weatherId={weatherData.sky} 
-                        country={weatherData.country}
-                        date={weatherData.date}
-                    />
-                ))}
-            </div>
-        </div>
-    );
+  function handleWeatherData(data) {
+    console.log(data[0]);
+    setWeatherDataList((oldList) => [...oldList, ...data]);
+  }
+  return (
+    <div className="app">
+      <h1 class="title">Byga</h1>
+      <SearchBox onWeatherData={handleWeatherData} />
+      <div className="card-container">
+        {weatherDataList.map((weatherData, index) => (
+          <WeatherCard
+            key={index}
+            city={weatherData.cityname}
+            temp={weatherData.temp}
+            weatherId={weatherData.sky}
+            country={weatherData.country}
+            date={weatherData.date}
+          />
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default App;
